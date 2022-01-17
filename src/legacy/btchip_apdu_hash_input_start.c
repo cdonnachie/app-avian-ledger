e90b20c592fd7e48b3c89ee93958714630dcabc7
kralverde <2b2tminecrafter2@gmail.com>
2022-01-17 14:31:24 -0500

    version 1.0

--- src/legacy/btchip_apdu_hash_input_start.c
+++ src/legacy/btchip_apdu_hash_input_start.c
@@ -63,12 +63,12 @@ unsigned short btchip_apdu_hash_input_start() {
         return BTCHIP_SW_INCORRECT_P1_P2;
     }
 
-    if ((G_io_apdu_buffer[ISO_OFFSET_P2] == P2_NEW) ||
-        (G_io_apdu_buffer[ISO_OFFSET_P2] == P2_NEW_SEGWIT)) {
+    if (G_io_apdu_buffer[ISO_OFFSET_P2] == P2_NEW) {
+        //(G_io_apdu_buffer[ISO_OFFSET_P2] == P2_NEW_SEGWIT)) {
         // btchip_context_D.transactionContext.consumeP2SH =
         // ((N_btchip.bkp.config.options & BTCHIP_OPTION_SKIP_2FA_P2SH) != 0);
         if (G_io_apdu_buffer[ISO_OFFSET_P1] == P1_FIRST) {
-            unsigned char usingSegwit = (G_io_apdu_buffer[ISO_OFFSET_P2] == P2_NEW_SEGWIT);
+            //unsigned char usingSegwit = (G_io_apdu_buffer[ISO_OFFSET_P2] == P2_NEW_SEGWIT);
                 
             // Request PIN validation
             // Only request PIN validation (user presence) to start a new
@@ -84,7 +84,7 @@ unsigned short btchip_apdu_hash_input_start() {
             btchip_context_D.transactionContext.firstSigned = 1;
             btchip_context_D.transactionContext.consumeP2SH = 0;
             btchip_context_D.transactionContext.relaxed = 0;
-            btchip_context_D.usingSegwit = usingSegwit;
+            btchip_context_D.usingSegwit = 0;
                 
             btchip_context_D.segwitParsedOnce = 0;
             btchip_set_check_internal_structure_integrity(1);
